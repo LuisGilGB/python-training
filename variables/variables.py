966db56ed793e265922c0d2bc8e466ee3281171c
LuisGilGB <luis.gilgb@gmail.com>
2020-07-02 21:32:17 +0200

    Variables

---
+++ variables/variables.py
@@ -0,0 +1,12 @@
+print('Declare a as number 5:')
+a = 5
+print('a:')
+print(a)
+print('a + 7:')
+print(a + 7)
+print('Declare b as string 5:')
+b = '5'
+print('b:')
+print(b)
+print('b + 7 (string):')
+print(b + '7')
