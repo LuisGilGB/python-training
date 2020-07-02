8904c42c7463a41209afbeb4915b383cb26069ed
LuisGilGB <luis.gilgb@gmail.com>
2020-07-02 21:54:24 +0200

    String training

---
+++ strings/comillaTriple.py
@@ -0,0 +1,8 @@
+# Ejemplo de uso de un triple par de comillas en print:
+print("""Estamos metiendo
+un salto
+de línea
+cada dos
+palabras,
+bueno, en ésta sólo una
+y ahora unas pocas más""")
---
+++ strings/tab.py
@@ -0,0 +1,4 @@
+print('-\tA\tB\tC')
+print('1\t1A\t1B\t1C')
+print('2\t2A\t2B\t2C')
+print('3\t3A\t3B\t3C')
