2c812671f82d02639731e3d7341c89511d9e687e
LuisGilGB <luis.gilgb@gmail.com>
2020-07-03 18:19:57 +0200

    Cast to set training

---
+++ sets/toSet.py
@@ -0,0 +1,6 @@
+myList = [1,3,6,3,3,5,6,4,6,2,4,6,8,9,4]
+print(myList)
+print(set(myList))
+myStr = 'jkgrfjgnkrjetgmkjhgr,jhbtcrkhjrtvbkklhgmctljhbljhmvrhjvgtcbjxhgrtljvb'
+print(myStr)
+print(set(myStr))
